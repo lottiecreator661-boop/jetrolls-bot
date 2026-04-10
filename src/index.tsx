7f0b873a0941ed8b6b9102a0c8b036f02745f25f
lottiecreator661-boop <lottiecreator661@gmail.com>
2026-04-10 17:50:21 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-// 1. Подключаем скрипт Telegram динамически
-const script = document.createElement("script");
-script.src = "https://telegram.org/js/telegram-web-app.js";
-script.async = true;
-script.onload = () => {
-  // 2. Когда скрипт загрузился, расширяем приложение
-  if (window.Telegram?.WebApp) {
-    window.Telegram.WebApp.ready();
-    window.Telegram.WebApp.expand();
-  }
-};
-document.head.appendChild(script);
-
-let rootElement = document.getElementById("root");
-if (!rootElement) {
-  rootElement = document.createElement("div");
-  rootElement.id = "root";
-  document.body.appendChild(rootElement);
-}
-
-const root = ReactDOM.createRoot(rootElement as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
